bc8bfd06980a8801946764695cf42ec7e5c2af3a
cuiwm <xcuiwm@hotmail.com>
2018-01-02 10:50:47 +0800

    apue_3e unix高级环境编程 unpv12  Unix网络编程API second Edition unpv13  Unix网络编程API 3rd Edition unpv22  进程通信IPC 卷2

---
+++ unpv13e/choeMake.sh
@@ -0,0 +1,28 @@
+#########################################################################
+# File Name: choeMake.sh
+# Author: andrew Choe
+# mail: xgod@163.com
+# Created Time: 一 12/25 20:07:30 2017
+#########################################################################
+#!/bin/bash
+ ./configure    # try to figure out all implementation differences
+
+    cd lib         # build the basic library that all programs need
+    make           # use "gmake" everywhere on BSD/OS systems
+
+    cd ../libfree  # continue building the basic library
+    make
+
+    cd ../libroute # only if your system supports 4.4BSD style routing sockets
+    make           # only if your system supports 4.4BSD style routing sockets
+
+    cd ../libxti   # only if your system supports XTI
+    make           # only if your system supports XTI
+
+    cd ../intro    # build and test a basic client program
+    make daytimetcpcli
+
+    #./daytimetcpcli 127.0.0.1
+	./daytimetcpcli 64.113.32.5
+
+echo "done"
